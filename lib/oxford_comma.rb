d9c4698b0984a8b4c14214d5b6f53e3ea99cef26
Luiz Pereira <luizfper@gmail.com>
2019-01-29 22:02:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
-  if array.length==2
-    return array.join(" and" )
-  elseif array==[]
-   return strlast
-  end
   strlast = array.pop
-
+  if array.length==2
+    return array.join(" and " )
+  elseif array==[]
+    return strlast
+  end
   str_array=array.join (", ")
   str_array << " and #{strlast}"
 end
