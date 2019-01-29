8c3ba72890c30506d6db803c9f4b7fa43b7d6db4
Luiz Pereira <luizfper@gmail.com>
2019-01-29 22:04:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length==2
+    return array.join(" and " )
+  elsif array.length==1
+    return array.join
+  end
+  strlast = array.pop
+  str_array=array.join (", ")
+  str_array << ", and #{strlast}"
 end
